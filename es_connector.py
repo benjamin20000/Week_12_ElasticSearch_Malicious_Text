b7fc28d5988a8ca33a84657e9cd7afbfacd51a54
benjamin <2000beni770@gmail.com>
2025-09-01 15:20:27 +0300

    update scema

--- es_connector.py
+++ es_connector.py
@@ -5,17 +5,22 @@ class EsConnect:
     def __init__(self):
         self.es = Elasticsearch("http://localhost:9200")
 
-    def create_schema(self):
+    def create_index(self):
         mapping = {
             "properties": {
                 "TweetID1": {"type": "float"},
                 "CreateDate": {"type": "text"},
                 "Antisemitic": {"type": "integer"},
-                "text": {"type": "text"}
+                "text": {"type": "text"},
+                "emotion":{"type":"text"},
+                "weapon_detected":{"type":"text"}
             }
         }
         index_name = "my_documents"
         self.es.indices.create(index=index_name, body={"mappings": mapping}, ignore=400)
+
+    def delete_index(self):
+        self.es.indices.delete(index="my_documents", ignore_unavailable=True)
 
     def insert_df(self,df):
         for index, row in df.iterrows():
